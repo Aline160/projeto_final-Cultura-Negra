3c0cd05cea3ea8949c91207a53ebe228376d0a9c
aline160 <aline.pereirasv2@gmail.com>
2020-11-29 20:38:37 -0300

    porta

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const port = process.env.PORT;
+const port = process.env.port;
 
 app.listen(port, () =>{
     console.log (`Executando em http://localhost: ${port}`);
