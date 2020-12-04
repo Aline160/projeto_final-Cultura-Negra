d4f67196c77603cefec38a40605932455de216df
aline160 <aline.pereirasv2@gmail.com>
2020-12-03 22:44:50 -0300

    porta

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "*");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
